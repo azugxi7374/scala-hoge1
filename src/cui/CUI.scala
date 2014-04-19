c4ffe56f43e739dacb34785035d9beedd02d896b
ixxa <ixxa.igusa@gmail.com>
2014-04-19 21:39:41 +0900

    first commit

---
+++ src/cui/CUI.scala
@@ -0,0 +1,27 @@
+package cui
+
+import scala.util.matching._
+
+/////////////////////////////////
+// CUI
+case class CUI(comlist : List[Command]) {
+
+	def run(str : String) {
+		println("run")
+		comlist.collectFirst(com => str match {
+			case com.reg(strlist @ _*) => com.run(strlist)
+		})
+	}
+}
+
+///////////////////////////////
+// Command
+case class Command(reg : Regex, run : Seq[String] => Any, help : String)
+
+
+
+
+
+
+
+
