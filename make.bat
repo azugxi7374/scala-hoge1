4ec018ed685eb5b2bdaadaa50bb74ff68ff49570
ixxa <ixxa.igusa@gmail.com>
2014-04-20 17:36:19 +0900

    batch test 5

--- make.bat
+++ make.bat
@@ -9,15 +9,15 @@ set /P IXXASCALALIBOK="COMPILE?"
 call sbt compile
 set /P IXXASCALALIBOK="PACKAGE?"
 call sbt package
-set /P IXXASCALALIBOK="GIT ADD?" & ^
-call git add . & ^
+set /P IXXASCALALIBOK="GIT ADD?"
+call git add .
 
-set /P IXXASCALALIBOK="COMMIT?" & ^
+set /P IXXASCALALIBOK="COMMIT?"
 set /P IXXASCALALIBCOMMES="commit message : "
-call git commit -m %IXXASCALALIBCOMMES% & ^
+call git commit -m %IXXASCALALIBCOMMES%
 
 set /P IXXASCALALIBOK="PUSH?"
-call git push origin master & ^
+call git push origin master
 
 set /P IXXASCALALIBOK="COPY?"
 call cp .\target\scala-2.10\ixxascalalib_2.10-0.01.jar %DROPBOX%\Public\liblary
