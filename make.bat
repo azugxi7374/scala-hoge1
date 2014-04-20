9f1f8bd17283f2774df764ce9ca9f4dc8405adc0
ixxa <ixxa.igusa@gmail.com>
2014-04-20 17:35:15 +0900

    batch test4

--- make.bat
+++ make.bat
@@ -2,23 +2,22 @@ echo off
 
 echo "compile -> package -> git add -> commit -> push -> cp to lib folder"
 
+set IXXASCALALIBOK=
 set IXXASCALALIBCOMMES=
+
+set /P IXXASCALALIBOK="COMPILE?"
+call sbt compile
+set /P IXXASCALALIBOK="PACKAGE?"
+call sbt package
+set /P IXXASCALALIBOK="GIT ADD?" & ^
+call git add . & ^
+
+set /P IXXASCALALIBOK="COMMIT?" & ^
 set /P IXXASCALALIBCOMMES="commit message : "
+call git commit -m %IXXASCALALIBCOMMES% & ^
 
-echo "--------------------------COMPILE-------------------------" & ^
-sbt compile & ^
+set /P IXXASCALALIBOK="PUSH?"
+call git push origin master & ^
 
-echo "--------------------------PACKAGE-------------------------" & ^
-sbt package & ^
-
-echo "--------------------------GIT ADD-------------------------" & ^
-git add . & ^
-
-echo "--------------------------COMMIT-------------------------" & ^
-git commit -m %IXXASCALALIBCOMMES% & ^
-
-echo "--------------------------PUSH-------------------------" & ^
-git push origin master & ^
-
-echo "--------------------------COPY-------------------------" & ^
-cp .\target\scala-2.10\ixxascalalib_2.10-0.01.jar %DROPBOX%\Public\liblary
+set /P IXXASCALALIBOK="COPY?"
+call cp .\target\scala-2.10\ixxascalalib_2.10-0.01.jar %DROPBOX%\Public\liblary
