68aca392eb8adcbfb593f1708d1ee526d5f97198
ixxa <ixxa.igusa@gmail.com>
2014-04-20 17:51:04 +0900

    add strClip

--- src/main/scala/ixxa/Hoge.scala
+++ src/main/scala/ixxa/Hoge.scala
@@ -6,6 +6,11 @@ object Hoge{
 	// キー± → 速度
 	def keySpd(x:Double) = Math.pow(2,x/12)
 
-
+	// string to clipboard
+	def strClip(str:String){
+		val clipboard = java.awt.Toolkit.getDefaultToolkit.getSystemClipboard
+		val sel = new java.awt.datatransfer.StringSelection(str)
+		clipboard.setContents(sel, sel)
+	}
 }
 
