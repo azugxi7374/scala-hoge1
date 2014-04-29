3db69047f3b9d51aa1f597c8ff829b1305e11854
ixxa <ixxa.igusa@gmail.com>
2014-04-29 22:23:35 +0900

    Seg, Rect

--- src/main/scala/ixxa/Hoge.scala
+++ src/main/scala/ixxa/Hoge.scala
@@ -1,12 +1,13 @@
 package ixxa
 
 import scala.language.implicitConversions
+import scala.annotation.tailrec
 
 // どうでもいいメソッド群
 object Hoge {
 
 	// キー± → 速度
-	def keyspd (x : Double) = Math.pow(2, x / 12)
+	def keyspd(x : Double) = Math.pow(2, x / 12)
 
 	implicit def toMyString[T](x : T) : MyString[T] = new MyString(x)
 
@@ -20,5 +21,44 @@ object Hoge {
 			clipboard.setContents(sel, sel)
 		}
 	}
+
+	// Seg
+	case class Seg(s : Int, e : Int) extends Seq[Int] {
+		def iterator = Range(s, e).iterator
+		def apply(i : Int) = Range(s, e)(i)
+		def length = Range(s, e).length
+		def ++(s2 : Seg) = Seg(Math.min(s, s2.s), Math.max(e, s2.e))
+		def **(s2 : Seg) = Seg(Math.max(s, s2.s), Math.min(e, s2.e))
+	}
+
+	// Rect
+	case class Rect(seg1 : Seg, seg2 : Seg) extends Seq[(Int, Int)] {
+		val (s1, e1, s2, e2) = (seg1.s, seg1.e, seg2.s, seg2.e)
+		def ++(r2 : Rect) = Rect(seg1 ++ r2.seg1, seg2 ++ r2.seg2)
+		def **(r2 : Rect) = Rect(seg1 ** r2.seg1, seg2 ** r2.seg2)
+		def iterator = seg1.iterator.flatMap { i1 => seg2.map { i2 => (i1, i2) } }
+		// TODO
+		def apply(i : Int) = iterator.toSeq(i)
+		// TODO
+		def length = iterator.length
+	}
+	object Rect {
+		def apply(s1 : Int, e1 : Int, s2 : Int, e2 : Int) : Rect = Rect(Seg(s1, e1), Seg(s2, e2))
+	}
 }
+
+final case class UnionFind(size : Int) {
+	val par = Array.tabulate(size) { i => i }
+
+	@tailrec
+	def root(i : Int) : Int = if (par(i) == i) i else root(par(i))
+
+	@tailrec
+	def union(i1 : Int, i2 : Int) : Unit = {
+		val p1 = par(i1)
+		par(i1) = root(i2)
+		if (p1 != i1) union(p1, i2)
+	}
+}
+
 
